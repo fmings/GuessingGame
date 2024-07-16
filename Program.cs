688fadbd6af12c95f4ba048ac6add7c7366f71ba
fmings <mingsfelicia@gmail.com>
2024-07-16 18:15:26 -0500

    phase Two complete

--- Program.cs
+++ Program.cs
@@ -3,9 +3,18 @@ Console.WriteLine("Guess a number between 1 and 100");
 string guessedNumberInput = Console.ReadLine();
 if (int.TryParse(guessedNumberInput, out int guessedNumber) && guessedNumber > 0 && guessedNumber <= 100)
 {
-    Console.WriteLine($"You guessed {guessedNumber}");
+    Console.WriteLine($"Let's see if you guessed correctly...");
+    if (guessedNumber == 42)
+    {
+        Console.WriteLine("Congratulations!! You guessed the secret number!");
+    }
+    else
+    {
+        Console.WriteLine("Tooooo bad...you guessed wrong!");
+    }
 }
 else
 {
     Console.WriteLine("Invalid guess. Please guess a number between 1 and 100.");
 }
+int secretNumber = 42;
